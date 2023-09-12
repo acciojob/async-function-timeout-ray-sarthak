9aff908302d6554c280c37fabbed37a1c16044be
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-12 23:04:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+
+async function display(){
+	const textvalue=document.getElementById("text").value;
+const time=document.getElementById("delay").value;
+	const outputDiv = document.getElementById('output');
+	await new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve()
+		},time)
+	})
+	outputDiv.textContent=textvalue;
+}
+document.getElementById('btn').addEventListener('click', display);
+
+//console.log(textvalue)
